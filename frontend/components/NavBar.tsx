ea0ad2e7c2146815c9ed947a7c6af85c4ee5cd0c
Juano <juanesuarez481@gmail.com>
2023-03-20 16:16:57 -0500

    Frontend as git submodule changed to folder

---
+++ frontend/components/NavBar.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState } from 'react'
+
+const NavBar = () => {
+    const [navActive, setNavActive] = useState(0);
+    return (
+        <header>
+            <nav className='description'>
+                <Link href="/">
+                    <h1>
+                        Cellier
+                    </h1>
+                </Link>
+                <div className='btncellier'
+                    onClick={() =>
+                        setNavActive(navActive)}>
+                    <Link href="/">
+                        <Image
+                            src="/settings.png"
+                            alt="settings"
+                            className='vercelLogo3'
+                            width={512}
+                            height={512}
+                            priority
+                        />
+                    </Link>
+                    <Link href="/">
+                        <Image
+                            src="/usuario.png"
+                            alt="usuario"
+                            className='vercelLogo2'
+                            width={512}
+                            height={512}
+                            priority
+                        />
+                    </Link>
+                    <Image
+                        src="/cellier-logo-square.png"
+                        alt="cellier-logo-square"
+                        className='vercelLogo1'
+                        width={512}
+                        height={512}
+                        priority
+                    />
+                </div>
+            </nav>
+        </header>
+    );
+}
+export default NavBar
